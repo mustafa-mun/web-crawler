bcf2bbb1e8cd9c0a1289c0eec4da4d4c7614c2bb
mustafa <mustafamcm9@gmail.com>
2023-05-17 11:49:23 +0300

    add normalizeURL function

--- crawl.js
+++ crawl.js
@@ -1,7 +1,17 @@
 function normalizeURL(url) {
-  // normalize url
+  const newURL = new URL(url);
+  // Sanitize the url
+  let sanitized = newURL.toString().toLowerCase();
+  // If url ends with '/'
+  if (sanitized.endsWith("/")) {
+    // remove the last char
+    sanitized = sanitized.slice(0, -1);
+  }
+  // Create new url with sanitized url and return normalized url
+  const sanitizedURL = new URL(sanitized);
+  return sanitizedURL.hostname + sanitizedURL.pathname;
 }
 
 module.exports = {
-  normalizeURL
-}
+  normalizeURL,
+};
--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,2 +1,20 @@
-const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('../crawl.js')
+const { test, expect } = require("@jest/globals");
+const { normalizeURL } = require("../crawl");
+
+expectedOutput = "twitter.com/home";
+
+test("works with http", () => {
+  expect(normalizeURL("http://twitter.com/home")).toBe(expectedOutput);
+});
+
+test("works with https", () => {
+  expect(normalizeURL("https://twitter.com/home")).toBe(expectedOutput);
+});
+
+test("works with capitalized letters", () => {
+  expect(normalizeURL("https://twiTter.Com/hOme")).toBe(expectedOutput);
+});
+
+test("works with extra slash at the end", () => {
+  expect(normalizeURL("https://twitter.com/home/")).toBe(expectedOutput);
+});
