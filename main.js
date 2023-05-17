a631562f903671b414b29feb1d0a0a01bb9c675e
mustafa <mustafamcm9@gmail.com>
2023-05-17 18:03:38 +0300

    add basic main function structure

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("test");
+function main() {
+  const args = process.argv.slice(2);
+  if (args.length !== 1) {
+    throw new Error("Argument arrays length should be 1!");
+  }
+  console.log(`Crawler is starting with URL: ${args[0]}`);
+}
+
+main();
